b2b4ef37d77922a242504615ddc2c53e92c5d0b9
Oladapo Omonayajo <o.omonayajo@gmail.com>
2017-07-13 04:10:06 +0100

    Login redirect works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private auth: AuthService) {}
+
+  isGuest(): boolean {
+    return !this.auth.loggedIn();
+  }
 
   logout() {
     console.log('Logout Clicked');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { NgModule } from '@angular/core';
 import { HttpModule, Http, XHRBackend, RequestOptions } from '@angular/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LocalStorageModule, LocalStorageService } from 'angular-2-local-storage';
+import { JwtHelper } from 'angular2-jwt';
 
 import { provideHttpfactory, LOCAL_STORAGE_PREFIX } from './shared/config';
+import { BaseProvider } from './shared/base.provider';
 import { NpvnService } from './shared/npvn.service';
 import { AuthService } from './shared/auth.service';
 import { AuthGuard } from './shared/auth.guard';
@@ -31,7 +33,7 @@ import { LoginModule } from './login/login.module';
     AppRoutingModule
   ],
   providers: [
-    NpvnService, AuthGuard, AuthService, Notifier,
+    BaseProvider, NpvnService, AuthGuard, AuthService, Notifier, JwtHelper,
     { provide: Http, useFactory: provideHttpfactory, deps: [ XHRBackend, RequestOptions, LocalStorageService ] }
   ],
   bootstrap: [AppComponent]
