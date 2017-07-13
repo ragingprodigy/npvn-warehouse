74c34421927b55ff52ab2f4fbb4a102503d31e56
Oladapo Omonayajo <o.omonayajo@gmail.com>
2017-07-13 04:59:55 +0100

    Added login Screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Config } from '../shared/config';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+  loginError: any = null;
 
-  constructor() { }
+  constructor(
+    private builder: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      email: [Config.EMAIL, Validators.compose([Validators.email, Validators.required])],
+      password: [Config.PASSWORD, Validators.required]
+    });
   }
 
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
+import { SharedModule } from '../shared';
+
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    NgbModule,
+    SharedModule,
     LoginRoutingModule
   ],
   declarations: [LoginComponent]
