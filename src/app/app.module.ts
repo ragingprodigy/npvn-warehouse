c37f2eb797a969e5712581b40b6c49e30badcc49
Oladapo Omonayajo <o.omonayajo@gmail.com>
2017-07-13 04:01:55 +0100

    Updated App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { LocalStorageModule, LocalStorageService } from 'angular-2-local-storage
 
 import { provideHttpfactory, LOCAL_STORAGE_PREFIX } from './shared/config';
 import { NpvnService } from './shared/npvn.service';
+import { AuthService } from './shared/auth.service';
+import { AuthGuard } from './shared/auth.guard';
+import { Notifier } from './shared/notifier';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +28,7 @@ import { AppRoutingModule } from './app-routing.module';
     AppRoutingModule
   ],
   providers: [
-    NpvnService,
+    NpvnService, AuthGuard, AuthService, Notifier,
     { provide: Http, useFactory: provideHttpfactory, deps: [ XHRBackend, RequestOptions, LocalStorageService ] }
   ],
   bootstrap: [AppComponent]
