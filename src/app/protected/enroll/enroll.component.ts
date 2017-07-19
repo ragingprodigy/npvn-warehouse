064dba93e3b87845fb678c389661bc699ea32dbb
Oladapo Omonayajo <o.omonayajo@gmail.com>
2017-07-19 18:17:57 +0100

    Check for Device in EnrollCOmponent

--- src/app/protected/enroll/enroll.component.ts
+++ src/app/protected/enroll/enroll.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { NpvnService } from '../../shared/npvn.service'
 
 @Component({
   selector: 'app-enroll',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./enroll.component.scss']
 })
 export class EnrollComponent implements OnInit {
+  form: FormGroup;
+  loading = true;
+  device: any;
+  message: string;
 
-  constructor() { }
+  constructor(
+    private builder: FormBuilder, private remote: NpvnService
+  ) { }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      imei: ['', Validators.required],
+      misdn: ['', Validators.required]
+    });
   }
 
+  checkIMEI($event) {
+    this.loading = true;
+    this.remote.getDevice(this.form.value.imei)
+    .subscribe(
+      (device) => {
+        this.loading = false;
+        this.device = device;
+      }, e => {
+        this.message = e;
+        this.loading = false;
+        setTimeout(() => {
+          this.message = null;
+        }, 5000);
+      }
+    );
+  }
 }
